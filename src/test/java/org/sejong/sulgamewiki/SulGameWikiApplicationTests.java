d70473202fb1f6d03ae18161c50cf6296f6f98ed
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-06-03 22:42:48 +0900

    Initial commit

---
+++ src/test/java/org/sejong/sulgamewiki/SulGameWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sejong.sulgamewiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulGameWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
