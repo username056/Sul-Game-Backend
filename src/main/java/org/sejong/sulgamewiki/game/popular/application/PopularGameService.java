52adb409921778331a34fbc4e7ad8a6c03822942
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-08 01:07:44 +0900

    fixed(game): 이전 로직 주석처리

--- src/main/java/org/sejong/sulgamewiki/game/popular/application/PopularGameService.java
+++ src/main/java/org/sejong/sulgamewiki/game/popular/application/PopularGameService.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.sejong.sulgamewiki.game.common.exception.GameException;
 import org.sejong.sulgamewiki.game.popular.domain.entity.PopularGame;
 import org.sejong.sulgamewiki.game.popular.domain.repository.PopularGameRepository;
-import org.sejong.sulgamewiki.game.popular.dto.PopularGameDto;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,14 +15,14 @@ import org.springframework.stereotype.Service;
 public class PopularGameService {
   private final PopularGameRepository popularGameRepository;
 
-  public PopularGameDto getPopularGame(long popularGameId) {
-    // Repository 단에서 Entity 가져오기
-    PopularGame popularGame = popularGameRepository.findById(popularGameId)
-        .orElseThrow(() -> new GameException(GAME_NOT_FOUND));
-
-    // DTO로 변환시키기 ( Custom Data )
-    PopularGameDto popularGameDto = PopularGameDto.fromEntity(popularGame);
-
-    return popularGameDto;
-  }
+//  public PopularGameDto getPopularGame(long popularGameId) {
+//    // Repository 단에서 Entity 가져오기
+//    PopularGame popularGame = popularGameRepository.findById(popularGameId)
+//        .orElseThrow(() -> new GameException(GAME_NOT_FOUND));
+//
+//    // DTO로 변환시키기 ( Custom Data )
+//    PopularGameDto popularGameDto = PopularGameDto.fromEntity(popularGame);
+//
+//    return popularGameDto;
+//  }
 }
