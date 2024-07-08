b76af51cf27ce27c20fa4e705a62e0524e841ae9
username056 <didtnsdl0406@gmail.com>
2024-07-08 20:34:47 +0900

    feat(admin): admin entity 정의

---
+++ src/main/java/org/sejong/sulgamewiki/admin/domain/entity/Admin.java
@@ -0,0 +1,44 @@
+package org.sejong.sulgamewiki.admin.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "admin")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime updatedAt;
+
+}
