1f399f39dc799c3904c303fb16c08f327167da72
username056 <didtnsdl0406@gmail.com>
2024-07-11 20:38:26 +0900

    fixed(admin): Admin Controller 위치 변경

--- src/main/java/org/sejong/sulgamewiki/notification/presentation/NotificationController.java
+++ src/main/java/org/sejong/sulgamewiki/notification/presentation/NotificationController.java
@@ -1,9 +1,12 @@
-package org.sejong.sulgamewiki.notification.domain.presentation;
+package org.sejong.sulgamewiki.notification.presentation;
 
 import org.sejong.sulgamewiki.notification.application.NotificationService;
 import org.sejong.sulgamewiki.notification.domain.entity.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,8 @@ public class NotificationController {
     @Autowired
     private NotificationService notificationService;
 
-    @GetMapping("/notifications")
-    public List<Notification> getNotifications() {
-        return notificationService.getAllNotifications();
+    @GetMapping("/members/{memberId}/notifications")
+    public Page<Notification> getNotificationsByMemberId(@PathVariable Long memberId, Pageable pageable) {
+        return notificationService.getNotificationsByMemberId(memberId, pageable);
     }
 }
