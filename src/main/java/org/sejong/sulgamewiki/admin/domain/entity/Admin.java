bd1733879c743428bbe73450f01ea86ce37c3505
username056 <didtnsdl0406@gmail.com>
2024-07-11 20:41:23 +0900

    fixed(admin): Admin엔티티에 BaseEntity 상속

--- src/main/java/org/sejong/sulgamewiki/admin/domain/entity/Admin.java
+++ src/main/java/org/sejong/sulgamewiki/admin/domain/entity/Admin.java
@@ -1,31 +1,24 @@
 package org.sejong.sulgamewiki.admin.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.*;
+import org.sejong.sulgamewiki.common.entity.BaseEntity;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "admin")
 @Getter
 @Setter
 @NoArgsConstructor
-@ToString
-@EntityListeners(AuditingEntityListener.class)
-public class Admin {
+public class Admin extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String username;
+    private String nickname;
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -33,12 +26,5 @@ public class Admin {
     @Column(nullable = false)
     private String password;
 
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column
-    private LocalDateTime updatedAt;
 
 }
