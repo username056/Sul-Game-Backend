6092592dc67d4f9da13c427462bf142bdbd31524
username056 <didtnsdl0406@gmail.com>
2024-07-11 20:25:54 +0900

    feat(notification): Error Source Enum 추가

--- src/main/java/org/sejong/sulgamewiki/notification/exception/NotificationErrorCode.java
+++ src/main/java/org/sejong/sulgamewiki/notification/exception/NotificationErrorCode.java
@@ -8,9 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationErrorCode implements ErrorCode {
-    NOTIFICATION_NOT_FOUND("알림이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-    INVALID_NOTIFICATION_REQUEST("잘못된 알림 요청입니다.", HttpStatus.BAD_REQUEST);
+    NOTIFICATION_NOT_FOUND("알림이 존재하지 않습니다.", HttpStatus.NOT_FOUND, ErrorSource.OTHERS),
+    INVALID_NOTIFICATION_REQUEST("잘못된 알림 요청입니다.", HttpStatus.BAD_REQUEST, ErrorSource.OTHERS);
 
     private final String message;
     private final HttpStatus status;
+    private final ErrorSource errorSource;
 }
