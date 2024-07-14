0e9578b793ffcf29bae07e51ff5d0adf376e2a7f
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-15 00:36:54 +0900

    refactor(security): URL List Static String[] 으로 묶어서 정리

--- src/main/java/org/sejong/sulgamewiki/common/config/WebSecurityConfig.java
+++ src/main/java/org/sejong/sulgamewiki/common/config/WebSecurityConfig.java
@@ -21,6 +21,22 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+  private static final String[] AUTH_WHITELIST = {
+      "/", // 기본화면
+      "/api/**", //FIXME: 일시적으로 전체 API 주소 허용 (삭제해야함)
+      "/api/signup", // 회원가입
+      "/api/login", // 로그인
+      "/docs/**", // Swagger
+      "/v3/api-docs/**" // Swagger
+  };
+
+  private static final String[] ALLOWED_ORIGINS = {
+      "https://api.sul-game.info",
+      "https://www.sul-game.info",
+      "http://220.85.169.165:8085",
+      "http://localhost:8080",
+      "http://10.0.2.2:8080"
+  };
   @Bean
   public WebSecurityCustomizer configure() {
     return (web) -> web.ignoring()
@@ -38,14 +54,7 @@ public class WebSecurityConfig {
             .formLogin(AbstractHttpConfigurer::disable)
 
             .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers(
-                    "/", // 기본화면
-                    "/api/**", //FIXME: 일시적으로 전체 API 주소 허용 (삭제해야함)
-                    "/api/signup", // 회원가입
-                    "/api/login", // 로그인
-                    "/docs/**", // Swagger
-                    "/v3/api-docs/**" // Swagger
-                ).permitAll()
+                .requestMatchers(AUTH_WHITELIST).permitAll()
 //                .requestMatchers(HttpMethod.GET, "/api/my-page").hasRole("USER") //FIXME: 예시 페이지
                 .anyRequest().authenticated()
             )
@@ -70,16 +79,8 @@ public class WebSecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOriginPatterns(
-        Arrays.asList(
-            "https://api.sul-game.info",
-            "https://www.sul-game.info",
-            "http://220.85.169.165:8085",
-            "http://localhost:8080",
-            "http://10.0.2.2:8080"));
-
-    configuration.setAllowedMethods(
-        Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    configuration.setAllowedOriginPatterns(Arrays.asList(ALLOWED_ORIGINS));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     configuration.setAllowCredentials(true);
     configuration.setAllowedHeaders(Collections.singletonList("*"));
     configuration.setMaxAge(3600L);
