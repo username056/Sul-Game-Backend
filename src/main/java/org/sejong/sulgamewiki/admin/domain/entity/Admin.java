ad6140c09a27c5bf722bf1e193a3db33ec39cce6
username056 <didtnsdl0406@gmail.com>
2024-07-15 03:15:55 +0900

    refactor(admin): Admin Entity가 BaseMember를 상속하도록 수정

--- src/main/java/org/sejong/sulgamewiki/admin/domain/entity/Admin.java
+++ src/main/java/org/sejong/sulgamewiki/admin/domain/entity/Admin.java
@@ -2,29 +2,16 @@ package org.sejong.sulgamewiki.admin.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.sejong.sulgamewiki.common.entity.BaseEntity;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
+import org.sejong.sulgamewiki.common.entity.BaseMember;
 
 @Entity
-@Table(name = "admin")
 @Getter
 @Setter
 @NoArgsConstructor
-public class Admin extends BaseEntity {
+@Table(name = "admin")
+@ToString(exclude = "password")
+public class Admin extends BaseMember {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String nickname;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
 
 
 }
