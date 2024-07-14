65a17f5cbd5b0aadb769050e350c4e5cc0df6d09
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-14 23:06:53 +0900

    fix(game): PopularGame 과 BaseGame 에 SuperBuilder 어노테이션 추가

--- src/main/java/org/sejong/sulgamewiki/common/entity/BaseGame.java
+++ src/main/java/org/sejong/sulgamewiki/common/entity/BaseGame.java
@@ -2,6 +2,7 @@ package org.sejong.sulgamewiki.common.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.sejong.sulgamewiki.member.domain.entity.Member;
 
 @MappedSuperclass
@@ -10,6 +11,7 @@ import org.sejong.sulgamewiki.member.domain.entity.Member;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@SuperBuilder
 public abstract class BaseGame extends BaseEntity {
 
   @Column(nullable = false, length = 100)
@@ -19,10 +21,10 @@ public abstract class BaseGame extends BaseEntity {
   private String description;
 
   @Column(nullable = false)
-  private int likes;
+  private int likes = 0;
 
   @Column(nullable = false)
-  private int views;
+  private int views = 0;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_id", nullable = false)
--- src/main/java/org/sejong/sulgamewiki/game/popular/domain/entity/PopularGame.java
+++ src/main/java/org/sejong/sulgamewiki/game/popular/domain/entity/PopularGame.java
@@ -1,13 +1,9 @@
 package org.sejong.sulgamewiki.game.popular.domain.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,14 +11,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 import org.sejong.sulgamewiki.common.entity.BaseGame;
+import org.sejong.sulgamewiki.game.popular.dto.request.CreatePopularGameRequest;
 import org.sejong.sulgamewiki.member.domain.entity.Member;
 
 @ToString
 @Entity
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class PopularGame extends BaseGame {
@@ -30,4 +28,14 @@ public class PopularGame extends BaseGame {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  public static PopularGame toEntity(Member member, CreatePopularGameRequest request) {
+    return PopularGame.builder()
+        .title(request.getDescription())
+        .description(request.getDescription())
+        .member(member)
+        .likes(0)
+        .views(0)
+        .build();
+  }
 }
