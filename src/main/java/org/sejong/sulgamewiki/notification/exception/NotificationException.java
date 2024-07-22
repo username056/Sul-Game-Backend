d5cd30feadfb9f9e183d9c1fee5ce6f447879fc1
username056 <didtnsdl0406@gmail.com>
2024-07-22 21:10:10 +0900

    refactor(exception): NotificationException에서 ErrorSource 경로 수정

--- src/main/java/org/sejong/sulgamewiki/notification/exception/NotificationException.java
+++ src/main/java/org/sejong/sulgamewiki/notification/exception/NotificationException.java
@@ -1,7 +1,7 @@
 package org.sejong.sulgamewiki.notification.exception;
 
 import org.sejong.sulgamewiki.common.exception.CustomException;
-import org.sejong.sulgamewiki.common.exception.ErrorSource;
+import org.sejong.sulgamewiki.common.exception.constants.ErrorSource;
 import org.springframework.http.HttpStatus;
 
 public class NotificationException extends CustomException {
