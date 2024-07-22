415c42b91f189ca803d8dcab041f9339e4d5a9b8
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-22 16:01:12 +0900

    feat(game): PopularGame 게시글 상세보기 API 구현

---
+++ src/main/java/org/sejong/sulgamewiki/game/popular/dto/response/GetPopularGameResponse.java
@@ -0,0 +1,31 @@
+package org.sejong.sulgamewiki.game.popular.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.sejong.sulgamewiki.game.popular.domain.entity.PopularGame;
+
+@Getter
+@Setter
+@Builder
+public class GetPopularGameResponse {
+  private long popularGameId;
+  private String title;
+  private String introduction;
+  private String description;
+  private int likes;
+  private int views;
+  private long memberId;
+
+  public static GetPopularGameResponse from(PopularGame popularGame) {
+    return GetPopularGameResponse.builder()
+        .popularGameId(popularGame.getId())
+        .title(popularGame.getTitle())
+        .introduction(popularGame.getIntroduction())
+        .description(popularGame.getDescription())
+        .likes(popularGame.getLikes())
+        .views(popularGame.getViews())
+        .memberId(popularGame.getMember().getId())
+        .build();
+  }
+}
