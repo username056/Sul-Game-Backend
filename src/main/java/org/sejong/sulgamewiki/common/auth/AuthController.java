c6d65e771b824d3e9ad2f5beab7ed3d92a8fd413
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-22 16:25:49 +0900

    refactor(auth, error): Auth 임시 로직 삭제, ErrorSource 위치변경

--- src/main/java/org/sejong/sulgamewiki/common/auth/AuthController.java
+++ src/main/java/org/sejong/sulgamewiki/common/auth/AuthController.java
@@ -1,12 +1,7 @@
 package org.sejong.sulgamewiki.common.auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-
-  @GetMapping("/user")
-  public
-
 }
