6663a03d0036c21bee3e9cc87c4da9e0b659af78
username056 <didtnsdl0406@gmail.com>
2024-07-22 21:15:22 +0900

    feat(member): member service 및 custom user details service 추가

--- src/main/java/org/sejong/sulgamewiki/member/application/CustomUserDetailsService.java
+++ src/main/java/org/sejong/sulgamewiki/member/application/CustomUserDetailsService.java
@@ -3,23 +3,26 @@ package org.sejong.sulgamewiki.member.application;
 import lombok.RequiredArgsConstructor;
 import org.sejong.sulgamewiki.member.domain.entity.Member;
 import org.sejong.sulgamewiki.member.domain.repository.MemberRepository;
-import org.sejong.sulgamewiki.member.dto.response.MemberResponseDto;
+import org.sejong.sulgamewiki.member.dto.response.MemberResponse;
 import org.sejong.sulgamewiki.member.exception.MemberErrorCode;
 import org.sejong.sulgamewiki.member.exception.MemberException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
   private final MemberRepository memberRepository;
 
-  public MemberResponseDto loadUserByUsername(String memberStringId) throws UsernameNotFoundException {
+  @Override
+  public UserDetails loadUserByUsername(String memberStringId) throws UsernameNotFoundException {
     Member member = memberRepository.findById(Long.parseLong(memberStringId))
         .orElseThrow(
             () -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
 
-    return MemberResponseDto.from(member);
+    return (UserDetails) MemberResponse.from(member);
   }
 }
--- src/main/java/org/sejong/sulgamewiki/member/application/MemberService.java
+++ src/main/java/org/sejong/sulgamewiki/member/application/MemberService.java
@@ -1,13 +1,36 @@
 package org.sejong.sulgamewiki.member.application;
 
 import lombok.RequiredArgsConstructor;
+import org.sejong.sulgamewiki.member.domain.entity.Member;
 import org.sejong.sulgamewiki.member.domain.repository.MemberRepository;
+import org.sejong.sulgamewiki.member.dto.request.MemberRequest;
+import org.sejong.sulgamewiki.member.exception.MemberErrorCode;
+import org.sejong.sulgamewiki.member.exception.MemberException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
   private final MemberRepository memberRepository;
 
+  public Member createMember(MemberRequest memberRequest) {
+    Member member = memberRequest.toEntity();
+    return memberRepository.save(member);
+  }
+
+  public Member getMember(Long id) {
+    return memberRepository.findById(id)
+        .orElseThrow(()
+            -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+  }
+
+  public Member updateMember(Long id, MemberRequest memberRequest) {
+    Member existingMember = memberRepository.findById(id)
+        .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+    existingMember.updateFromRequest(memberRequest);
+    return memberRepository.save(existingMember);
+  }
 
 }
