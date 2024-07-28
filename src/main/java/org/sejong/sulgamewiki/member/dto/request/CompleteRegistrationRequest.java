81995b6c0aae0a0cfe29fce5b1a80774f4b1d44d
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-29 02:02:23 +0900

    feat(member): CompleteRegistrationRequest DTO 구현

---
+++ src/main/java/org/sejong/sulgamewiki/member/dto/request/CompleteRegistrationRequest.java
@@ -0,0 +1,16 @@
+package org.sejong.sulgamewiki.member.dto.request;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class CompleteRegistrationRequest {
+  private Long memberId;
+  private String nickName;
+  private LocalDate birthDate;
+  private String university;
+  private Boolean isUniversityVisible;
+  private Boolean isNotiEnabled;
+}
