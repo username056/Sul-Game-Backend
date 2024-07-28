b547798b5578a2be70fb60fe6e75580aa57bc39b
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-07-29 02:01:37 +0900

    fix(member): 이메일 삭제 및 memberId 가져오는 로직추가

--- src/main/java/org/sejong/sulgamewiki/member/dto/request/CreateMemberRequest.java
+++ src/main/java/org/sejong/sulgamewiki/member/dto/request/CreateMemberRequest.java
@@ -1,16 +1,16 @@
 package org.sejong.sulgamewiki.member.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import lombok.Getter;
 
 
 @Getter
 public class CreateMemberRequest {
-  private String email;
+  private Long memberId;
   private String nickName;
   private LocalDate birthDate;
   private String university;
   private Boolean isUniversityVisible;
   private Boolean isNotiEnabled;
-
 }
