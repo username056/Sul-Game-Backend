2f333d914a324cd1d01e4b03e8cc6e24de5a0af5
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-08-01 23:04:31 +0900

    feat(S3Service): MultiPart파일 업로드, 삭제 구현
    MultiPart파일 업로드, 삭제 구현(리스트형식으로 받아올 수 있게 수정)

--- src/main/java/org/sejong/sulgamewiki/common/aws_s3/application/S3Service.java
+++ src/main/java/org/sejong/sulgamewiki/common/aws_s3/application/S3Service.java
@@ -3,6 +3,8 @@ package org.sejong.sulgamewiki.common.aws_s3.application;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -21,17 +23,52 @@ public class S3Service {
   private String bucket;
 
   public String uploadFile(MultipartFile file, String category) throws IOException {
-    // 카테고리 값이 없거나 유효하지 않은 경우 예외 처리
-    if (category == null || category.isEmpty()) {
-      throw new IllegalArgumentException("Category must not be null or empty");
-    }
+    // 파일 이름 생성
+    String fileName = generateFileName(file, category);
 
-    // 카테고리를 소문자로 변환하여 파일명에 추가
-    String fileName = category.toLowerCase() + "-" + UUID.randomUUID() + "-" + file.getOriginalFilename();
     amazonS3.putObject(
         new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
             .withCannedAcl(CannedAccessControlList.PublicRead));
+
     return amazonS3.getUrl(bucket, fileName).toString();
+  }
+
+  // List<MulipartFile>
+  public List<String> uploadFiles(List<MultipartFile> files, String category) throws IOException{
+    List<String> fileUrls = new ArrayList<>();
+    for (MultipartFile file : files) {
+      fileUrls.add(uploadFile(file, category));
+    }
+    return fileUrls;
+  }
+
+  // delete
+  public void deleteFile(String fileUrl) throws IOException {
+    amazonS3.deleteObject(bucket, fileUrl);
+  }
+
+  // deleteFiles List<String>
+  public void deleteFiles(List<String> fileUrls) throws IOException{
+    for (String fileUrl : fileUrls) {
+      deleteFile(fileUrl);
+    }
+  }
+
+  // 파일 이름 생성 메소드
+  private String generateFileName(MultipartFile file, String category) {
+    if (category == null || category.isEmpty()) {
+      throw new IllegalArgumentException("Category must not be null or empty");
+    }
+    return category.toLowerCase() + "-" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+  }
+
+  // URL에서 파일 이름 추출 메소드
+  private String extractFileNameFromUrl(String fileUrl) {
+    int lastSlashIndex = fileUrl.lastIndexOf('/');
+    if (lastSlashIndex == -1) {
+      throw new IllegalArgumentException("Invalid file URL");
+    }
+    return fileUrl.substring(lastSlashIndex + 1);
   }
 
 }
