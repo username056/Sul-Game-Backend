66bd113991b0e8fd5980137b4a6c5af0969ad672
username056 <84067473+username056@users.noreply.github.com>
2024-08-05 23:52:40 +0900

    feat(common): GlobalErrorCode Enum Type 추가

--- src/main/java/org/sejong/sulgamewiki/common/exception/GlobalErrorCode.java
+++ src/main/java/org/sejong/sulgamewiki/common/exception/GlobalErrorCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum GlobalErrorCode implements ErrorCode{
-  INTERNAL_SERVER_ERROR("서버에 오류가 발생하였습니다.", HttpStatus.NOT_FOUND);
-
+  INTERNAL_SERVER_ERROR("서버에 오류가 발생하였습니다.", HttpStatus.BAD_REQUEST),
+  POST_NOT_FOUND("포스트를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
   private final String message;
   private final HttpStatus status;
 
