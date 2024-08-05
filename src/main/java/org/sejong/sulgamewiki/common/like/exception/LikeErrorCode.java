c595fc77dc63bdd8c0e0a3e9ec8a0c5e80400927
username056 <84067473+username056@users.noreply.github.com>
2024-08-05 23:57:18 +0900

    feat(like): LikeErrorCode에 EnumType추가

--- src/main/java/org/sejong/sulgamewiki/common/like/exception/LikeErrorCode.java
+++ src/main/java/org/sejong/sulgamewiki/common/like/exception/LikeErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum LikeErrorCode implements ErrorCode {
   LIKE_CANNOT_BE_UNDER_ZERO("좋아요는 음수일 수 없습니다.", HttpStatus.BAD_REQUEST),
-  INVALID_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+  INVALID_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
+  LIKE_NOT_FOUND("해당 멤버의 좋아요가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
 
   private final String message;
   private final HttpStatus status;
