4d9e1f16c60f4fda8ce37f120b0a2568e3e2211e
username056 <84067473+username056@users.noreply.github.com>
2024-08-05 23:56:11 +0900

    refactor(intro): 엔티티 아이디 값 삭제 및 리펙토링

--- src/main/java/org/sejong/sulgamewiki/intro/domain/entity/Intro.java
+++ src/main/java/org/sejong/sulgamewiki/intro/domain/entity/Intro.java
@@ -1,11 +1,14 @@
 package org.sejong.sulgamewiki.intro.domain.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.sejong.sulgamewiki.common.entity.BasePost;
 import org.sejong.sulgamewiki.intro.dto.request.CreateIntroRequest;
@@ -18,11 +21,8 @@ import org.sejong.sulgamewiki.member.domain.entity.Member;
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@DiscriminatorValue("Intro")
 public class Intro extends BasePost {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long Id;
 
   @Column(length = 200)
   private String lyrics;
