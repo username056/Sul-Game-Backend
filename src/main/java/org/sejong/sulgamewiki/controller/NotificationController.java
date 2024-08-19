25cf1fd6267e88c172209c6eaad8a4c8c57af538
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-08-19 21:10:34 +0900

    fix(noti): 주석 해제 및 findByMemberMemberId 메서드 이름 변경

--- src/main/java/org/sejong/sulgamewiki/controller/NotificationController.java
+++ src/main/java/org/sejong/sulgamewiki/controller/NotificationController.java
@@ -1,26 +1,26 @@
-//package org.sejong.sulgamewiki.controller;
-//
-//import org.sejong.sulgamewiki.util.log.LogMonitoringInvocation;
-//import org.sejong.sulgamewiki.service.NotificationService;
-//import org.sejong.sulgamewiki.object.Notification;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class NotificationController {
-//
-//    @Autowired
-//    private NotificationService notificationService;
-//
-//    @GetMapping("/members/{memberId}/notifications")
-//    @LogMonitoringInvocation
-//    public Page<Notification> getNotificationsByMemberId(@PathVariable Long memberId, Pageable pageable) {
-//        return notificationService.getNotificationsByMemberId(memberId, pageable);
-//    }
-//}
+package org.sejong.sulgamewiki.controller;
+
+import org.sejong.sulgamewiki.util.log.LogMonitoringInvocation;
+import org.sejong.sulgamewiki.service.NotificationService;
+import org.sejong.sulgamewiki.object.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @GetMapping("/members/{memberId}/notifications")
+    @LogMonitoringInvocation
+    public Page<Notification> getNotificationsByMemberId(@PathVariable Long memberId, Pageable pageable) {
+        return notificationService.getNotificationsByMemberId(memberId, pageable);
+    }
+}
