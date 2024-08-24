51634b368d6de00d3d5cad62f8372cdff8ca15d3
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-08-25 02:23:26 +0900

    feat(test): 회원 정상 생성 확인 테스트

--- src/test/java/org/sejong/sulgamewiki/repository/MemberRepositoryTest.java
+++ src/test/java/org/sejong/sulgamewiki/repository/MemberRepositoryTest.java
@@ -1,8 +1,6 @@
 package org.sejong.sulgamewiki.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.sejong.sulgamewiki.object.Member;
@@ -14,6 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MemberRepositoryTest {
@@ -29,24 +30,34 @@ class MemberRepositoryTest {
 
   @Test
   @Transactional
+  public void mainTest() {
+    testCreateAndFindMockMember();
+  }
+
   void testCreateAndFindMockMember() {
+    log.info("회원 정상 생성 확인");
     // Mock 데이터 생성
     MockDto mockDto = mockService.createMockMember();
-    System.out.println("======");
-    System.out.println(mockDto.toString());
-    System.out.println("======");
+
+    log.info("생성된 가짜회원 ID: {}", mockDto.getMember().getMemberId());
+    log.info("생성된 가짜회원 Email: {}", mockDto.getMember().getEmail());
+
+    log.info("생성된 회원 활동 정보 ID: {}", mockDto.getMemberContentInteraction().getId());
+    log.info("생성된 회원 활동 정보의 회원 ID: {}", mockDto.getMemberContentInteraction().getMember().getMemberId());
 
     // 저장된 Member 엔터티 검증
     Member savedMember = memberRepository.findById(mockDto.getMember().getMemberId()).orElse(null);
 
-    System.out.println("======");
-    System.out.println(savedMember.toString());
-    System.out.println("======");
-
     // 저장된 MemberContentInteraction 엔터티 검증
     MemberContentInteraction savedInteraction = memberContentInteractionRepository.findByMember(savedMember);
-    System.out.println("======");
-    System.out.println(savedInteraction.toString());
-    System.out.println("======");
+
+    log.info("조회한 가짜회원 ID: {}", mockDto.getMember().getMemberId());
+    log.info("조회한 가짜회원 Email: {}", mockDto.getMember().getEmail());
+
+    log.info("조회한 회원 활동 정보 ID: {}", mockDto.getMemberContentInteraction().getId());
+    log.info("조회한 회원 활동 정보의 회원 ID: {}", mockDto.getMemberContentInteraction().getMember().getMemberId());
+
+    assertEquals(mockDto.getMember(), savedMember, "회원정보 일치 확인");
+    assertEquals(mockDto.getMemberContentInteraction(), savedInteraction, "Created and Saved MemberContentInteractions should be equal");
   }
 }
