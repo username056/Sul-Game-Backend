14d2a510cdba681faf1e55102701a5ce34959c43
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-08-25 23:51:17 +0900

    feat(baseMedia): 미디어 관련 메서드 추가

--- src/main/java/org/sejong/sulgamewiki/repository/BaseMediaRepository.java
+++ src/main/java/org/sejong/sulgamewiki/repository/BaseMediaRepository.java
@@ -1,5 +1,6 @@
 package org.sejong.sulgamewiki.repository;
 
+import java.util.List;
 import org.sejong.sulgamewiki.object.BaseMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BaseMediaRepository extends JpaRepository<BaseMedia, Long> {
 
+  List<String> findMediaUrlsByBasePost_BasePostId(Long basePostId);
 }
