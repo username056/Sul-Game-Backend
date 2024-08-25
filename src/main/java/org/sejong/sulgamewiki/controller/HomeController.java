cecd6db2ecb79d9ce2078b3a1e0420398aa4a045
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-08-25 21:18:03 +0900

    fix(HomeController): 인자로 HomeCommand로 수정

--- src/main/java/org/sejong/sulgamewiki/controller/HomeController.java
+++ src/main/java/org/sejong/sulgamewiki/controller/HomeController.java
@@ -1,6 +1,7 @@
 package org.sejong.sulgamewiki.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sejong.sulgamewiki.object.HomeCommand;
 import org.sejong.sulgamewiki.object.HomeDto;
 import org.sejong.sulgamewiki.service.HomeService;
 import org.springframework.data.domain.Pageable;
@@ -17,8 +18,8 @@ public class HomeController {
   private final HomeService homeService;
 
   @GetMapping
-  public ResponseEntity<HomeDto> getHomeData(Pageable pageable) {
+  public ResponseEntity<HomeDto> getHomeData(HomeCommand command) {
     // HomeDto를 반환하는 서비스 로직 호출
-    return ResponseEntity.ok(homeService.getHomeData(pageable));
+    return ResponseEntity.ok(homeService.getHomeData(command));
   }
 }
