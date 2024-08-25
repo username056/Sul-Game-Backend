32ce431288abb42300df63c83a571a016aa6cbc5
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-08-25 23:52:52 +0900

    feat(comment): 특정 게시물로 댓글을 찾는 메서드 추가

--- src/main/java/org/sejong/sulgamewiki/repository/CommentRepository.java
+++ src/main/java/org/sejong/sulgamewiki/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package org.sejong.sulgamewiki.repository;
 
+import java.util.List;
+import org.sejong.sulgamewiki.object.BasePost;
 import org.sejong.sulgamewiki.object.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  List<Comment> findByBasePost(BasePost basePost);
 }
