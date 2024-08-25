c44d486c5a2cbee028d62485c61bb784e66ad04e
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-08-25 12:34:26 +0900

    feat(CreativeGameCommand): 창작게임 엔티티

---
+++ src/main/java/org/sejong/sulgamewiki/object/CreativeGameCommand.java
@@ -0,0 +1,14 @@
+package org.sejong.sulgamewiki.object;
+
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
+public class CreativeGameCommand {
+  private Long memberId;
+  private Long basePostId;
+  private String title;
+  private String description;
+  private String introduction;
+  private List<String> imageUrls;
+  private List<MultipartFile> multipartFiles;
+}
