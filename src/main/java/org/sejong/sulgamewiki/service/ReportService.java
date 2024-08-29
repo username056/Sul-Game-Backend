5b639ebafd3b6ae186eecece72c962bb9440beac
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-08-29 23:08:12 +0900

    feat(report): reportService생성 및 게시물,댓글 신고 로직 작성

--- src/main/java/org/sejong/sulgamewiki/service/ReportService.java
+++ src/main/java/org/sejong/sulgamewiki/service/ReportService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.sejong.sulgamewiki.object.BasePost;
 import org.sejong.sulgamewiki.object.Comment;
 import org.sejong.sulgamewiki.object.Member;
+import org.sejong.sulgamewiki.object.OfficialGame;
 import org.sejong.sulgamewiki.object.Report;
 import org.sejong.sulgamewiki.object.ReportCommand;
 import org.sejong.sulgamewiki.object.ReportDto;
@@ -90,5 +91,55 @@ public class ReportService {
     return reportRepository.existsByReporterAndSourceIdAndSourceType(member, sourceId, sourceType);
   }
 
+  // 게시물 신고 로직
+  public ReportDto reportBasePost(ReportCommand reportCommand) {
+    BasePost basePost = basePostRepository.findById(reportCommand.getSourceId())
+        .orElseThrow(() -> new CustomException(ErrorCode.GAME_NOT_FOUND));
+
+    Member member = memberRepository.findById(reportCommand.getMemberId())
+        .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+    // 중복 신고 여부 확인
+    boolean isAlreadyReported = isAlreadyReported(
+        member, reportCommand.getSourceId(), reportCommand.getSourceType());
+    if (isAlreadyReported) {
+      throw new CustomException(ErrorCode.ALREADY_REPORTED);
+    }
+
+    // 리포트 생성
+    ReportCommand command = ReportCommand.builder()
+        .memberId(member.getMemberId())
+        .sourceId(basePost.getBasePostId())
+        .sourceType(reportCommand.getSourceType())
+        .reportType(reportCommand.getReportType())
+        .build();
+
+    return createReport(command);
+  }
+
+  // 댓글 신고 로직
+  public ReportDto reportComment(ReportCommand reportCommand) {
+    Comment comment = commentRepository.findById(reportCommand.getSourceId())
+        .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
+
+    Member member = memberRepository.findById(reportCommand.getMemberId())
+        .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+    // 중복 신고 여부 확인
+    boolean isAlreadyReported = isAlreadyReported(
+        member, reportCommand.getSourceId(), reportCommand.getSourceType());
+    if (isAlreadyReported) {
+      throw new CustomException(ErrorCode.ALREADY_REPORTED);
+    }
+
+    ReportCommand command = ReportCommand.builder()
+        .memberId(member.getMemberId())
+        .sourceId(comment.getCommentId())
+        .sourceType(reportCommand.getSourceType())
+        .reportType(reportCommand.getReportType())
+        .build();
+
+    return createReport(command);
+  }
 
 }
