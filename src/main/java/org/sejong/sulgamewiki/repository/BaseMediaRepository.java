efaa029cd76252a37452dc457fbe424fd3d8a706
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-08-31 03:03:29 +0900

    fix(BasePostRepository): BasePostRepository 및 BaseMediaRepository 수정
    isDeleted 모두 명시적으로 나타내기 위해 쿼리문 작성
    이거 전체 디폴트값으로 필터링 할수 있따는데 나중에 찾아볼래

--- src/main/java/org/sejong/sulgamewiki/repository/BaseMediaRepository.java
+++ src/main/java/org/sejong/sulgamewiki/repository/BaseMediaRepository.java
@@ -3,10 +3,21 @@ package org.sejong.sulgamewiki.repository;
 import java.util.List;
 import org.sejong.sulgamewiki.object.BaseMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BaseMediaRepository extends JpaRepository<BaseMedia, Long> {
 
+  @Query("SELECT entity.mediaUrl FROM BaseMedia entity WHERE entity.basePost.basePostId = :basePostId AND entity.isDeleted = false")
   List<String> findMediaUrlsByBasePost_BasePostId(Long basePostId);
+
+  @Query("SELECT entity FROM BaseMedia entity WHERE entity.basePost.basePostId = :basePostId AND entity.isDeleted = false")
+  List<BaseMedia> findAllByBasePost_BasePostId(Long basePostId);
+
+  @Query("SELECT entity FROM BaseMedia entity WHERE entity.basePost.basePostId = :basePostId AND entity.isDeleted = false")
+  BaseMedia findByMediaUrl(String mediaUrl);
+
+  @Query("SELECT entity FROM BaseMedia entity WHERE entity.mediaUrl = :mediaUrl AND entity.isDeleted = false")
+  List<BaseMedia> findByBasePost_BasePostId(Long basePostId);
 }
