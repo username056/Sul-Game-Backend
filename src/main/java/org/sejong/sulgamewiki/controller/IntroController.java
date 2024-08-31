b65f2f804c7bc68935f32cbc0723e88410f483a4
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-08-31 18:23:58 +0900

    fix(IntroController): 전체 조회 삭제
    테스트 내일 만들게요

--- src/main/java/org/sejong/sulgamewiki/controller/IntroController.java
+++ src/main/java/org/sejong/sulgamewiki/controller/IntroController.java
@@ -39,31 +39,21 @@ public class IntroController implements IntroControllerDocs{
     return ResponseEntity.ok(introService.createIntro(command));
   }
 
-  @GetMapping("/{basePostId}")
-  @Operation(summary = "인트로 조회", description = "특정 인트로를 조회합니다.")
-  public ResponseEntity<BasePostDto> getIntro(@PathVariable Long basePostId) {
-    BasePostDto intro = introService.getIntro(basePostId);
-    return ResponseEntity.ok(intro);
+  // TODO: URL 정석 지피티한테 물어보자
+  @PostMapping("/details")
+  public ResponseEntity<BasePostDto> getIntro(@ModelAttribute BasePostCommand command) {
+    return ResponseEntity.ok(introService.getIntro(command));
   }
 
-  @GetMapping
-  @Operation(summary = "인트로 전체 조회", description = "모든 인트로를 조회합니다.")
-  public ResponseEntity<List<BasePostDto>> getAllIntros() {
-    List<BasePostDto> intros = introService.getAllIntros();
-    return ResponseEntity.ok(intros);
-  }
-
-  @PutMapping(value = "/{basePostId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  @Operation(summary = "인트로 수정", description = "기존 인트로를 수정합니다.")
+  // TODO: URL 정석 지피티한테 물어보자. (이게 맞다네)
+  @PutMapping(name = "/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<BasePostDto> updateIntro(@ModelAttribute BasePostCommand command) {
-    BasePostDto updatedIntro = introService.updateIntro(command);
-    return ResponseEntity.ok(updatedIntro);
+    return ResponseEntity.ok(introService.updateIntro(command));
   }
 
-  @DeleteMapping("/{basePostId}")
-  @Operation(summary = "인트로 삭제", description = "특정 인트로를 소프트 삭제합니다.")
-  public ResponseEntity<Void> deleteIntro(@PathVariable Long basePostId) {
-    introService.deleteIntro(basePostId);
+  @DeleteMapping("/")
+  public ResponseEntity<Void> deleteIntro(@ModelAttribute BasePostCommand command) {
+    introService.deleteIntro(command);
     return ResponseEntity.noContent().build();
   }
 }
