fb8577b9eb57aec3078274a5623fbd99333d73d1
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-09-02 03:14:16 +0900

    feat(IntroService): 전체조회 삭제 및 엔티티Set

--- src/main/java/org/sejong/sulgamewiki/service/IntroService.java
+++ src/main/java/org/sejong/sulgamewiki/service/IntroService.java
@@ -1,10 +1,13 @@
 package org.sejong.sulgamewiki.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.sejong.sulgamewiki.object.BaseMedia;
+import org.sejong.sulgamewiki.object.BasePost;
 import org.sejong.sulgamewiki.object.BasePostCommand;
 import org.sejong.sulgamewiki.object.BasePostDto;
 import org.sejong.sulgamewiki.object.Intro;
@@ -57,13 +60,12 @@ public class IntroService {
 
   // Read: 특정 Intro 조회
   @Transactional(readOnly = true)
-  public BasePostDto getIntro(Long basePostId) {
-    Intro intro = (Intro) basePostRepository.findByBasePostId(basePostId);
-    if (intro == null) {
-        throw new CustomException(ErrorCode.POST_NOT_FOUND);
-    }
+  public BasePostDto getIntro(BasePostCommand command) {
 
-    List<BaseMedia> medias = basePostRepository.findMediasByBasePostId(basePostId);
+    Intro intro = basePostRepository.findIntroByBasePostId(command.getBasePostId())
+        .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+
+    List<BaseMedia> medias = basePostRepository.findMediasByBasePostId(command.getBasePostId());
 
     return BasePostDto.builder()
         .basePost(intro)
@@ -71,31 +73,24 @@ public class IntroService {
         .build();
   }
 
-  @Transactional(readOnly = true)
-  public List<BasePostDto> getAllIntros() {
-    List<Intro> intros = basePostRepository.findAllIntros();  // findAllIntros 메서드 구현 필요
-    return intros.stream().map(intro -> {
-      List<BaseMedia> medias = baseMediaRepository.findAllByBasePost_BasePostId(intro.getBasePostId());
-      return BasePostDto.builder()
-          .basePost(intro)
-          .baseMedias(medias)
-          .build();
-    }).collect(Collectors.toList());
-  }
-
   @Transactional
   public BasePostDto updateIntro(BasePostCommand command) {
     // 기존 Intro 게시글 조회
-    Intro existingIntro = (Intro) basePostRepository.findByBasePostId(command.getBasePostId());
-    if (existingIntro == null) {
-      throw new CustomException(ErrorCode.POST_NOT_FOUND);
-    }
+    Intro existingIntro = (Intro) basePostRepository.findByBasePostId(command.getBasePostId())
+            .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
-    // 게시글의 제목, 소개글, 가사, 설명 등을 업데이트
+    // 게시글의 제목, 가사, 설명 등을 업데이트
     existingIntro.setTitle(command.getTitle());
-    existingIntro.setIntroduction(command.getIntroduction());
     existingIntro.setLyrics(command.getLyrics());
     existingIntro.setDescription(command.getDescription());
+    existingIntro.setType(command.getIntroType());
+    existingIntro.setIntroTags(command.getIntroTags());
+    existingIntro.setThumbnailIcon(command.getThumbnailIcon());
+
+    // 업데이트 표시
+    existingIntro.markAsUpdated();
+
+    command.setBasePost(existingIntro);
 
     // 연관된 미디어 파일 업데이트
     List<BaseMedia> updatedMedias = baseMediaService.updateMedias(command);
@@ -113,15 +108,12 @@ public class IntroService {
 
   // Delete: 특정 Intro 삭제 (소프트 삭제, 미디어 파일은 진짜 삭제)
   @Transactional
-  public void deleteIntro(Long basePostId) {
-    Intro intro = (Intro) basePostRepository.findByBasePostId(basePostId);
-
-    if (intro == null) {
-      throw new CustomException(ErrorCode.POST_NOT_FOUND);
-    }
+  public void deleteIntro(BasePostCommand command) {
+    Intro intro = (Intro) basePostRepository.findByBasePostId(command.getBasePostId())
+        .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
     // 연관된 미디어 파일을 실제로 삭제
-    List<BaseMedia> medias = baseMediaRepository.findAllByBasePost_BasePostId(basePostId);
+    List<BaseMedia> medias = baseMediaRepository.findAllByBasePost_BasePostId(command.getBasePostId());
     if (medias != null && !medias.isEmpty()) {
       baseMediaService.deleteMedias(medias);
     }
