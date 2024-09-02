789284ae7456801f07cbfb0fd76675cda9156e83
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-09-02 22:47:04 +0900

    fix(Tag): 태그수정

--- src/main/java/org/sejong/sulgamewiki/object/constants/IntroTag.java
+++ src/main/java/org/sejong/sulgamewiki/object/constants/IntroTag.java
@@ -1,18 +1,30 @@
 package org.sejong.sulgamewiki.object.constants;
 
+import org.sejong.sulgamewiki.object.Intro;
+
 public enum IntroTag {
-  이성과하기좋아요,
-  빨리취해요,
-  움직여야해요,
-  머리를써야해요,
-  스킨쉽있어요,
-  엄청빨라요,
-  차분해요,
-  금방끝나요,
-  오래걸려요,
-  눈치챙겨,
-  센스가필요해요,
-  도구가필요해요,
-  저격할수있어요,
-  텐션올릴때좋아요
+  TAG1("이성과 하기 좋아요"),
+  TAG2("빨리 취해요"),
+  TAG3("움직여야 해요"),
+  TAG4("머리를 써야 해요"),
+  TAG5("스킨쉽 있어요"),
+  TAG6("엄청 빨라요"),
+  TAG7("차분해요"),
+  TAG8("금방 끝나요"),
+  TAG9("오래 걸려요"),
+  TAG10("눈치 챙겨요"),
+  TAG11("센스가 필요해요"),
+  TAG12("도구가 필요해요"),
+  TAG13("저격할 수 있어요"),
+  TAG14("텐션 올릴 때 좋아요");
+
+  private final String description;
+
+  IntroTag(String description) {
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
 }
