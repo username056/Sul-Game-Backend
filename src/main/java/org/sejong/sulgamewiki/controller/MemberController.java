20835c4027bde8e148299182a04789fa3cce0294
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-09-02 23:11:29 +0900

    feat(security): 스프링 시큐리티 추가

--- src/main/java/org/sejong/sulgamewiki/controller/MemberController.java
+++ src/main/java/org/sejong/sulgamewiki/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.sejong.sulgamewiki.util.auth.CustomUserDetails;
 import org.sejong.sulgamewiki.util.log.LogMonitoringInvocation;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
