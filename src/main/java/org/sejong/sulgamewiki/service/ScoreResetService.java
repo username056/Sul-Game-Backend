ef606f4d42fa6c3ffd99c0572b3ebe3b966468ec
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-09-02 15:06:47 +0900

    fix(ScoreResetService): 비동기식 처리를 위한 어노테이션 추가

--- src/main/java/org/sejong/sulgamewiki/service/ScoreResetService.java
+++ src/main/java/org/sejong/sulgamewiki/service/ScoreResetService.java
@@ -3,6 +3,7 @@ package org.sejong.sulgamewiki.service;
 import java.util.List;
 import org.sejong.sulgamewiki.object.BasePost;
 import org.sejong.sulgamewiki.repository.BasePostRepository;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ public class ScoreResetService {
 
   // 매일자정마다 실행 (cron 표현식을 사용하여 스케줄링)
   @Scheduled(cron = "0 0 0 1/1 * ? *")
+  @Async
   public void resetDailyScores() {
     List<BasePost> posts = basePostRepository.findAll();
     for (BasePost post : posts) {
@@ -25,6 +27,7 @@ public class ScoreResetService {
 
   // 일요일자정마다 실행 (매일 자정에 실행)
   @Scheduled(cron = "0 0 0 ? * SUN *")
+  @Async
   public void resetWeeklyScores() {
     List<BasePost> posts = basePostRepository.findAll();
     for (BasePost post : posts) {
