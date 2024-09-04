8f6afc0950991fecb6d5beebd2a643fa3d73c361
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-09-04 19:34:02 +0900

    feat(officialGame): create수정, delete, update메서드 추가

--- src/main/java/org/sejong/sulgamewiki/controller/OfficialGameController.java
+++ src/main/java/org/sejong/sulgamewiki/controller/OfficialGameController.java
@@ -10,9 +10,13 @@ import org.sejong.sulgamewiki.service.OfficialGameService;
 import org.sejong.sulgamewiki.util.log.LogMonitoringInvocation;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +27,15 @@ import org.springframework.web.bind.annotation.RestController;
     name = "국룰 게임 관리 API",
     description = "국룰 게임 관리 API 제공"
 )
-public class OfficialGameController implements OfficialGameControllerDocs{
+public class OfficialGameController implements OfficialGameControllerDocs {
   private final OfficialGameService officialGameService;
 
-  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   @LogMonitoringInvocation
   public ResponseEntity<BasePostDto> createOfficialGame(
-      @ModelAttribute BasePostCommand command
-  ) {
+      @AuthenticationPrincipal UserDetails userDetails,
+      @ModelAttribute BasePostCommand command) {
+    command.setMemberId(Long.parseLong(userDetails.getUsername()));
     return ResponseEntity.ok(officialGameService.createOfficialGame(command));
   }
 
@@ -38,8 +43,23 @@ public class OfficialGameController implements OfficialGameControllerDocs{
   @LogMonitoringInvocation
   public ResponseEntity<BasePostDto> getPopularGame(
       @ModelAttribute BasePostCommand command) {
-    BasePostDto dto = officialGameService.getOfficialGame(command);
-    return ResponseEntity.ok(dto);
+    return ResponseEntity.ok(officialGameService.getOfficialGame(command));
   }
-  
+
+  @PutMapping(name = "/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<BasePostDto> updateOfficialGame(
+      @AuthenticationPrincipal UserDetails userDetails,
+      @ModelAttribute BasePostCommand command) {
+    command.setMemberId(Long.parseLong(userDetails.getUsername()));
+    return ResponseEntity.ok(officialGameService.updateOfficialGame(command));
+  }
+
+  @DeleteMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<Void> deleteOfficialGame(
+      @AuthenticationPrincipal UserDetails userDetails,
+      @ModelAttribute BasePostCommand command) {
+    command.setMemberId(Long.parseLong(userDetails.getUsername()));
+    officialGameService.deleteOfficialGame(command);
+    return ResponseEntity.noContent().build();
+  }
 }
