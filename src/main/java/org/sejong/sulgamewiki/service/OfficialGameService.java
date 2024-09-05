1e2ef6c1a1712325ab5e01384e4ebc1d7818d450
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-09-06 00:06:31 +0900

    feat(officialGame): Creat메서드 수정

--- src/main/java/org/sejong/sulgamewiki/service/OfficialGameService.java
+++ src/main/java/org/sejong/sulgamewiki/service/OfficialGameService.java
@@ -1,8 +1,6 @@
 package org.sejong.sulgamewiki.service;
 
 
-import static org.sejong.sulgamewiki.object.BasePost.checkCreatorInfoIsPrivate;
-
 import java.util.HashSet;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -43,6 +41,7 @@ public class OfficialGameService {
    * @return
    */
 
+  @Transactional
   public BasePostDto createOfficialGame(BasePostCommand command) {
 
     Member member = memberRepository.findById(command.getMemberId())
@@ -71,10 +70,12 @@ public class OfficialGameService {
     command.setBasePost((savedOfficialGame));
 
     List<BaseMedia> savedMedias = baseMediaService.uploadMediasFromGame(command);
+    BaseMedia savedIntroMedia = baseMediaService.uploadIntroMediaFromGame(command);
 
     return BasePostDto.builder()
         .officialGame(savedOfficialGame)
         .baseMedias(savedMedias)
+        .introMediaFromGame(savedIntroMedia)
         .build();
   }
 
@@ -88,12 +89,16 @@ public class OfficialGameService {
     List<BaseMedia> medias = basePostRepository.findMediasByBasePostId(
         command.getBasePostId());
 
+    BaseMedia introMediaFileInGamePost = baseMediaRepository.findByMediaUrl(command.getIntroMediaFileInGamePostUrl());
+
     return BasePostDto.builder()
         .officialGame(officialGame)
         .baseMedias(medias)
+        .introMediaFromGame(introMediaFileInGamePost)
         .build();
   }
 
+  @Transactional
   public BasePostDto updateOfficialGame(BasePostCommand command) {
 
     OfficialGame existingOfficialGame = basePostRepository.findOfficialGameByBasePostId(
@@ -111,22 +116,29 @@ public class OfficialGameService {
     existingOfficialGame.setTitle(command.getTitle());
     existingOfficialGame.setIntroduction(command.getIntroduction());
     existingOfficialGame.setDescription(command.getDescription());
+    existingOfficialGame.setIntroLyricsInGamePost(command.getIntroLyricsInGamePost());
+    // 인트로 미디어
     existingOfficialGame.setThumbnailIcon(command.getThumbnailIcon());
     existingOfficialGame.setGameTags(command.getGameTags());
+    existingOfficialGame.setCreatorInfoPrivate(command.getIsCreatorInfoPrivate());
+
 
     existingOfficialGame.markAsUpdated();
     command.setBasePost(existingOfficialGame);
 
     List<BaseMedia> updatedMedias = baseMediaService.updateMedias(command);
     // 게시글과 미디어 파일 저장
+    BaseMedia updatedIntroMediaFromGame = baseMediaService.updateMedias(command).get(0);
+
     basePostRepository.save(existingOfficialGame);
 
     return BasePostDto.builder()
         .officialGame(existingOfficialGame)
-        .baseMedias(updatedMedias)
+ //       .baseMedias(updatedIntroMediaFromGame)
         .build();
   }
 
+  @Transactional
   public void deleteOfficialGame(BasePostCommand command) {
     OfficialGame officialGame = basePostRepository.findOfficialGameByBasePostId(
             command.getBasePostId())
@@ -139,36 +151,18 @@ public class OfficialGameService {
       throw new CustomException(ErrorCode.UNAUTHORIZED_ACTION);
     }
 
+    // 연관된 미디어 파일을 실제로 삭제
+    List<BaseMedia> medias = baseMediaRepository.findAllByBasePost_BasePostId(
+        command.getBasePostId());
+    if (medias != null && !medias.isEmpty()) {
+      baseMediaService.deleteMedias(medias);
+    }
+
     // 게시글을 삭제된 것으로 표시
     officialGame.markAsDeleted();
+
     // 변경사항을 저장
     basePostRepository.save(officialGame);
   }
 
-
-//  public ReportDto reportGame(ReportCommand reportCommand) {
-//    OfficialGame officialGame = (OfficialGame) basePostRepository.findById(reportCommand.getSourceId())
-//        .orElseThrow(() -> new CustomException(ErrorCode.GAME_NOT_FOUND));
-//
-//    Member member = memberRepository.findById(reportCommand.getMemberId())
-//        .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-//
-//    // 중복 신고 여부 확인
-//    boolean isAlreadyReported = reportService.isAlreadyReported(
-//        member, reportCommand.getSourceId(), SourceType.OFFICIAL_GAME);
-//    if (isAlreadyReported) {
-//      throw new CustomException(ErrorCode.ALREADY_REPORTED);
-//    }
-//
-//    // 리포트 생성
-//    ReportCommand command = ReportCommand.builder()
-//        .memberId(member.getMemberId())
-//        .sourceId(officialGame.getBasePostId())
-//        .sourceType(SourceType.OFFICIAL_GAME)
-//        .reportType(reportCommand.getReportType())
-//        .build();
-//
-//    reportService.createReport(command);
-//    return null;
-//  }
 }
