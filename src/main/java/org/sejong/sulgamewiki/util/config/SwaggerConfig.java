97f4a5cf8332a9af15b333177c5bc57053d64060
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2024-09-07 22:49:50 +0900

    feat(swagger): Swagger Docs UI 개선, 및 로그인 주소 추가, 테스트 URL 추가

--- src/main/java/org/sejong/sulgamewiki/util/config/SwaggerConfig.java
+++ src/main/java/org/sejong/sulgamewiki/util/config/SwaggerConfig.java
@@ -12,12 +12,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-    info = @Info(title = "술겜위키 : SULGAME WIKI",
-        description = "다함께 만들어가는 술게임 백과사전",
+    info = @Info(title = "🍻 술겜위키 : SULGAME WIKI 🍻",
+        description = """
+            **술게임의 모든 것을 여기서 만나보세요!**  
+            다양한 술게임을 확인하고, 직접 만든 술게임과 인트로를 공유할 수 있는 플랫폼
+            
+            ### 🔗 **빠르게 로그인하기:**
+            - 🔐 **[메인 서버 로그인](https://api.sul-game.info/login)**  
+            
+            - 🔐 **[로컬 서버 로그인](http://localhost:8080/login)**  
+              
+            - 🔐 **[테스트 서버 로그인](https://test.sul-game.info/login)**  
+
+            ### 📲 **앱 다운로드 및 정보:**
+            - 📥 **[술겜위키 APK 다운로드](http://220.85.169.165/sul-game/)**  
+              > 최신 버전의 APK 파일을 다운로드하고 술게임 정보를 쉽게 접하세요.
+            
+            ### 💻 **GitHub 저장소:**
+            - 🛠️ **[백엔드 소스코드](https://github.com/Sul-Game-Wiki/Sul-Game-Backend)**  
+              > 백엔드 관련 소스코드를 확인하고 기여해보세요
+              
+            - 🎨 **[프론트엔드 소스코드](https://github.com/Sul-Game-Wiki/Sul-Game-Frontend)**  
+              > 프론트엔드 디자인 및 로직을 확인해보세요
+            """,
         version = "0.1v"),
     servers = {
-        @Server(url = "https://api.sul-game.info", description = "Main Sever"),
-        @Server(url = "http://localhost:8080", description = "Local Server")
+        @Server(url = "https://api.sul-game.info", description = "메인 서버"),
+        @Server(url = "http://localhost:8080", description = "로컬 서버"),
+        @Server(url = "https://test.sul-game.info", description = "테스트 서버")
     }
 )
 @Configuration
