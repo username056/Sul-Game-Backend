492db94ed5c1106405799eae8f505b61957c02b7
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-09-15 15:37:03 +0900

    refactor(officialGame): 컨트롤러 문서 수정

--- src/main/java/org/sejong/sulgamewiki/controller/OfficialGameControllerDocs.java
+++ src/main/java/org/sejong/sulgamewiki/controller/OfficialGameControllerDocs.java
@@ -13,36 +13,47 @@ public interface OfficialGameControllerDocs {
   @Operation(
       summary = "공식 게임 생성",
       description = """
+          **토큰 필요**
+            
           **공식 게임 생성**
 
           새로운 공식 게임 게시물을 생성합니다.
 
           **입력 파라미터 값:**
-         
-          - **`Long memberId`**: 게시물을 생성하는 회원의 고유 ID
+        
           - **`String title`**: 게임의 제목
-            _최대 000자_
+            _최대 20자_
+            
           - **`String introduction`**: 게임의 소개 텍스트
-            _최대 000자_
+            _최대 20자_
+            
           - **`String description`**: 게임에 대한 상세 설명
-            _최대 000자_
+            _최대 1000자_
+            
           - **`String introLyrics`**: 게임의 인트로의 텍스트
-            _최대 000자_
+            _최대 150자_
+            
           - **`MultipartFile introMedia`**: 게임의 인트로의 미디어 파일
             _선택 사항_
+            
           - **`List<MultipartFile> multipartFiles`**: 게임과 관련된 미디어 파일들
             _선택 사항_
+            
           - **`Object<Icon>`**: 게임에 대한 아이콘
             _선택 사항_
+            
           - **`List<String> tags`**: 게임에 대한 태그
             _선택 사항_
+            
           - **`Boolean IsCreatorInfoPrivate`**: 작성자 정보 공개 여부
-            _선택 사항_
+            _선택 사항_ 기본값이 공개
+            
           
           **반환 파라미터 값:**
 
           - **`OfficialGame officialGame`**: 생성된 공식 게임 게시물
           - **`List<BaseMedia> baseMedias`**: 게임과 연관된 미디어 파일 리스트
+          - **`BaseMedia introMediaInGame`**: 게임 인트로 미디어 파일
           """
   )
   ResponseEntity<BasePostDto> createOfficialGame(
@@ -64,6 +75,8 @@ public interface OfficialGameControllerDocs {
 
           - **`OfficialGame officialGame`**: 조회된 공식 게임 게시물
           - **`List<BaseMedia> baseMedias`**: 게임과 연관된 미디어 파일 리스트
+          - **`BaseMedia introMediaInGame`**: 게임 인트로 미디어 파일
+          - **`List<CreationGame> creationGames`**: 연관된 창작 게임 리스트
           """
   )
   ResponseEntity<BasePostDto> getOfficialGame(@ModelAttribute BasePostCommand command);
@@ -71,34 +84,46 @@ public interface OfficialGameControllerDocs {
   @Operation(
       summary = "공식 게임 수정",
       description = """
-          **공식 게임 수정**
+         **토큰 필요**
+          
+         **공식 게임 수정**
 
           기존 공식 게임 게시물을 수정합니다.
 
-         - **`Long memberId`**: 게시물을 생성하는 회원의 고유 ID
-          - **`String title`**: 게임의 제목
-            _최대 000자_
-          - **`String introduction`**: 게임의 소개 텍스트
-            _최대 000자_
-          - **`String description`**: 게임에 대한 상세 설명
-            _최대 000자_
-          - **`String introLyrics`**: 게임의 인트로의 텍스트
-            _최대 000자_
-          - **`MultipartFile introMedia`**: 게임의 인트로의 미디어 파일
+         **입력 파라미터 값**
+        
+         - **`String title`**: 게임의 제목
+            _최대 20자_
+            
+         - **`String introduction`**: 게임의 소개 텍스트
+            _최대 20자_
+            
+         - **`String description`**: 게임에 대한 상세 설명
+            _최대 1000자_
+            
+         - **`String introLyrics`**: 게임의 인트로의 텍스트
+            _선택사항_ _최대 150자_
+            
+         - **`MultipartFile introMedia`**: 게임의 인트로의 미디어 파일
             _선택 사항_
-          - **`List<MultipartFile> multipartFiles`**: 게임과 관련된 미디어 파일들
+            
+         - **`List<MultipartFile> multipartFiles`**: 게임과 관련된 미디어 파일들
             _선택 사항_
-          - **`Object<Icon>`**: 게임에 대한 아이콘
+            
+         - **`Object<Icon>`**: 게임에 대한 아이콘
             _선택 사항_
-          - **`List<String> tags`**: 게임에 대한 태그
+            
+         - **`List<String> tags`**: 게임에 대한 태그
             _선택 사항_
-          - **`Boolean IsCreatorInfoPrivate`**: 작성자 정보 공개 여부
-            _선택 사항_
-
+            
+         - **`Boolean IsCreatorInfoPrivate`**: 작성자 정보 공개 여부
+            _선택 사항_ 기본값이 공개
+            
           **반환 파라미터 값:**
 
           - **`OfficialGame officialGame`**: 생성된 공식 게임 게시물
           - **`List<BaseMedia> baseMedias`**: 게임과 연관된 미디어 파일 리스트
+          - **`BaseMedia introMediaInGame`**: 게임 인트로 미디어 파일
           """
   )
   ResponseEntity<BasePostDto> updateOfficialGame(
