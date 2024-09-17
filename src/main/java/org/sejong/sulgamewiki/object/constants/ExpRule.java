4f4010505ab463b0cdac9f8dcb35295358cc0945
Mr-Tongyoung <154494090+Mr-Tongyoung@users.noreply.github.com>
2024-09-17 23:17:32 +0900

    fix(ExpRule): 변수명 수정으로 주체와 객체 분리

--- src/main/java/org/sejong/sulgamewiki/object/constants/ExpRule.java
+++ src/main/java/org/sejong/sulgamewiki/object/constants/ExpRule.java
@@ -16,13 +16,16 @@ public enum ExpRule {
 
   // 게시글 관련
   POST_CREATION(20L, "게시글 작성 시 20 경험치 부여"),
-  POST_LIKE_RECEIVED(5L, "게시글에 좋아요를 받으면 5 경험치 부여"),
-  POST_LIKE_GIVEN(1L, "게시글에 좋아요를 누르면 1 경험치 부여"),
+  POST_LIKE_GIVEN(5L, "게시글에 좋아요를 받으면 5 경험치 부여"),
+  POST_LIKE_CANCELED(-5L, "게시글에 좋아요가 취소되면 5 경험치 회수"),
+  POST_LIKE_GIVE(1L, "게시글에 좋아요를 누르면 1 경험치 부여"),
+  POST_LIKE_CANCEL(-1L, "게시글에 좋아요를 취소하면 1 경험치 회수"),
   POST_DELETION(-20L, "게시글 삭제 시 20 경험치 회수"),
 
   // 댓글 관련
   COMMENT_CREATION(2L, "댓글 작성 시 2 경험치 부여"),
   COMMENT_LIKE_RECEIVED(1L, "댓글에 좋아요를 받으면 1 경험치 부여"),
+  COMMENT_LIKE_CANCELED(1L, "댓글에 좋아요가 취소되면 1 경험치 회수"),
   COMMENT_DELETION(-2L, "댓글 삭제 시 2 경험치 회수"),
 
   // 신고 관련
