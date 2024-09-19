3052ddc0768604aec6bfe85096153d62a2cc4bbf
tillthelight-p <84067473+username056@users.noreply.github.com>
2024-09-20 00:38:45 +0900

    refactor(notification): 사용하지 않는 로직 주석 처리

--- src/main/java/org/sejong/sulgamewiki/repository/NotificationRepository.java
+++ src/main/java/org/sejong/sulgamewiki/repository/NotificationRepository.java
@@ -1,13 +1,12 @@
-package org.sejong.sulgamewiki.repository;
-
-import org.sejong.sulgamewiki.object.Notification;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    Page<Notification> findByMemberMemberId(Long memberId, Pageable pageable);
-}
+//package org.sejong.sulgamewiki.repository;
+//
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//
+//@Repository
+//public interface NotificationRepository extends JpaRepository<Notification, Long> {
+//    Page<Notification> findByMemberMemberId(Long memberId, Pageable pageable);
+//}
